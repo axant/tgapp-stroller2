c46ee393a85f4202813b0c446543a42177dd3d9f
sergio brero <sergio.brero.it@gmail.com>
2020-01-13 08:56:05 +0100

    wip on tests

---
+++ sample-ecommerce/sampleecommerce/tests/functional/test_stroller2_manage_category.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from nose.tools import eq_, ok_
+from sampleecommerce.tests import TestController
+from tgext.ecommerce.model.models import Category
+from sampleecommerce.model import DBSession
+
+
+class TestManageCategoryController(TestController):
+
+    def setup(self):
+        super(TestManageCategoryController, self).setUp()
+        self.category = Category(
+            name={'en': 'Categoria 1'},
+            slug='cat-1',
+            details='details',
+            sort_weight=1
+        )
+        DBSession.flush()
+
+    def test_category_index(self):
+        response = self.app.get(
+            '/commerce/manage/category',
+            extra_environ=self.admin_environ,
+            status=200
+        )
+        assert 'Categoria 1' in response.body.decode('utf-8')
+
+    def test_create_category(self):
+        response = self.app.get(
+            '/commerce/manage/category/new',
+            extra_environ=self.admin_environ,
+            status=200
+        )
+
+        form = response.form
+        form['name'] = 'Categoria 2'
+        form['parent_id'] = str(self.category._id)
+
+        submission = form.submit(
+            extra_environ=self.admin_environ,
+            status=302
+        )
+        redirection = submission.follow(
+            extra_environ=self.admin_environ
+        )
+        assert 'Category created' in redirection.body.decode('utf-8')
